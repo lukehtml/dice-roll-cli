8ef9762ee0bcde829023494d2302688547f67f10
lukery <kedoooooooooooo@gmail.com>
2026-03-06 18:56:26 +0100

    add total rolled
    Added total roll calculation and output to the roll command.

--- dice.py
+++ dice.py
@@ -8,9 +8,12 @@ def roll(dice_input: str):
     times = dice[0]
     dice = dice[1]
     rolled = 0
+    total = 0
     for i in range(0, int(times)):
         rolled = random.randint(1, int(dice))
+        total += rolled
         print(f"You rolled a [bold magenta]{rolled}[/bold magenta]")
+    print(f"You rolled a total of [bold green]{total}[/bold green]")
 
 if __name__ == "__main__":
     app()
